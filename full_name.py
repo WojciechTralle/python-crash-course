26d9debe1bb46576d944ef220754ee1fdfe1b152
Wojciech Tralle, Ph.D. <122245444+WojciechTralle@users.noreply.github.com>
2026-06-21 16:00:13 -0400

    Add files via upload

---
+++ full_name.py
@@ -0,0 +1,23 @@
+first_name = "ada"
+last_name = "lovelace"
+#Here's an example of an f-string:
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+#full_name = "{} {}".format(first_name, last_name)
+#print(full_name)
+
+
+print("Python")
+print("\tPython")
+print("\t\tPython")
+print("\t\t\tPython")
+
+print("Programming Languages:\nPython\nC\nJavaScript")
+print("Programming Languages:\n\tPython\n\tC\n\tJavaScript")
+
+print('python:')
+print('python :')
+
+
