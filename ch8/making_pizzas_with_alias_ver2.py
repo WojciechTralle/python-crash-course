0a2e23dd3188566489882f778f0f192ef6c06249
Wojciech Tralle <122245444+WojciechTralle@users.noreply.github.com>
2026-07-09 00:08:28 -0400

    more stuff from Matthes + probability simulations

---
+++ ch8/making_pizzas_with_alias_ver2.py
@@ -0,0 +1,5 @@
+import pizza_modified_include_size as p
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
---
+++ ch8/making_pizzas_with_alias_ver3.py
@@ -0,0 +1,5 @@
+from pizza_modified_include_size import *
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
